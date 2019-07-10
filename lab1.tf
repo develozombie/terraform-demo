822cf2cadd3db8795ede757cb98f22d58a61cc31
Jose Yapur <yapur@outlook.com>
2019-07-09 19:37:52 -0500

    Create lab1.tf

---
+++ lab1.tf
@@ -0,0 +1,18 @@
+locals {
+  subscription_id= ""
+  client_id= ""
+  client_secret= ""
+  tenant_id= ""
+  proyecto= ""
+  ambiente= ""
+}
+provider "azurerm" {
+  subscription_id= "${local.subscription_id}"
+  client_id = "${local.client_id}"
+  client_secret = "${local.client_secret}"
+  tenant_id = "${local.tenant_id}"
+}
+resource "azurerm_resource_group" "rgid" {
+  name     = "RGEU2${local.proyecto}${local.ambiente}"
+  location = "East US 2"
+}
