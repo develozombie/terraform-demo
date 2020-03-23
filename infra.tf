d05c20461e32f4fce68e248a6742890523ec0b29
Jose Yapur <yapur@outlook.com>
2020-03-23 11:49:51 -0500

    Update infra.tf

--- infra.tf
+++ infra.tf
@@ -1,9 +1,8 @@
 provider "azurerm" {
+  # whilst the `version` attribute is optional, we recommend pinning to a given version of the Provider
+  features {}
 }
-resource "azurerm_resource_group" "rgid" {
-  name     = "RGEU2${local.proyecto}${local.ambiente}"
-  location = "East US 2"
-  tags = {
-    Ambiente = "Desarrollo"
-  }
+resource "azurerm_resource_group" "test" {
+  name     = "acctestRG1"
+  location = "East US"
 }
