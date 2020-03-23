7f9e5394b1d2d58c108f62f97d6a746621e65b0e
Jose Yapur <jose@gremlin.lan>
2020-03-23 18:21:56 -0500

    clase 1

--- parte2/infra.tf
+++ parte2/infra.tf
@@ -3,15 +3,15 @@ provider "azurerm" {
   features {}
 }
 resource "azurerm_resource_group" "test" {
-  name     = "RGAzureDemo"
-  location = "East US"
+  name     = "RG${local.alias}"
+  location = "${local.region}"
   tags = {
     Ambiente = "Desarrollo"
   }
 }
 
 resource "azurerm_app_service_plan" "main" {
-  name                = "azdemojoyapu-asp"
+  name                = "az${local.alias}-asp"
   location            = "${azurerm_resource_group.test.location}"
   resource_group_name = "${azurerm_resource_group.test.name}"
 
@@ -22,7 +22,7 @@ resource "azurerm_app_service_plan" "main" {
 }
 
 resource "azurerm_app_service" "main" {
-  name                = "azdemojoyapu-web"
+  name                = "az${local.alias}-web"
   location            = "${azurerm_resource_group.test.location}"
   resource_group_name = "${azurerm_resource_group.test.name}"
   app_service_plan_id = "${azurerm_app_service_plan.main.id}"
